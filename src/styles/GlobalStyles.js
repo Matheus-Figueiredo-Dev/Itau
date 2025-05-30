ef3c4bc464126d150f481aa0d6792a164c7c09ed
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-05-30 12:41:10 -0300

    First commit

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlogalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: "Lato", sans-serif;
+    }
+`;
